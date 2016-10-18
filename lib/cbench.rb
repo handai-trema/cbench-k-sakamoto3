0943baa4d901a7711b56147471ca0ccf1dc7e047
k-sakamoto3 <moi1990sk@gmail.com>
2016-10-19 03:01:08 +0900

    ruby-prof追加

--- lib/cbench.rb
+++ lib/cbench.rb
@@ -1,15 +1,20 @@
 # A simple openflow controller for benchmarking.
+require 'ruby-prof'
+
 class Cbench < Trema::Controller
   def start(_args)
     logger.info 'Cbench started.'
   end
 
   def packet_in(datapath_id, message)
+    RubyPorf.start
     send_flow_mod_add(
       datapath_id,
       match: ExactMatch.new(message),
       buffer_id: message.buffer_id,
       actions: SendOutPort.new(message.in_port + 1)
     )
+    result = RubyProf.stop
+    RubyProf::FlatPrinter.new(result).print(STDOUT)
   end
 end
